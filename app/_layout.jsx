02b5ebb7d4f917ab4a4e9756c035e28320b4a939
Ngoni-Sendama <ngonidzashesendama@gmail.com>
2025-09-27 21:43:52 +0530

    Dark and Light theme with colors

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,24 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, useColorScheme } from 'react-native'
 import React from 'react'
 import { Slot, Stack } from 'expo-router'
+import { Colors } from "../constants/Colors"
+import { StatusBar } from 'expo-status-bar'
 
 const RoorLayout = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
     return (
+       <>
+       <StatusBar value="auto"/>
         <Stack screenOptions={{
-            headerStyle: {backgroundColor: '#ddd'},
-            headerTintColor: '#333'
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title
         }}>
-            <Stack.Screen name="index" options={{title: 'Home'}}/>
-            <Stack.Screen name="about" options={{title: 'About'}}/>
-            <Stack.Screen name="contact" options={{title: 'Contact', headerShown: false}}/>
+            <Stack.Screen name="index" options={{ title: 'Home' }} />
+            <Stack.Screen name="about" options={{ title: 'About' }} />
+            <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} />
         </Stack>
+       </>
     )
 }
 
--- app/about.jsx
+++ app/about.jsx
@@ -1,10 +1,13 @@
-import {  StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useColorScheme } from 'react-native'
 import React from 'react'
-import {Link} from 'expo-router'
+import { Link } from 'expo-router'
+import { Colors } from "../constants/Colors"
 
 const About = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
       <Text>About</Text>
       <Link href="/" style={styles.link}>Back Home</Link>
     </View>
@@ -14,14 +17,14 @@ const About = () => {
 export default About
 
 const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    link: {
-        marginVertical: 10,
-        borderBottomWidth:1
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
 
-    }
+  }
 })
